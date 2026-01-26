660867018d61e10a1a3d941c43e1c9928e3b3792
Peter Myllykoski <peter.myllykoski@situ.se>
2026-01-27 00:45:21 +0100

    CORS middleware and test added

---
+++ src/middleware/CorsMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SSA\middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class CorsMiddleware implements MiddlewareInterface
+{
+    private array $config;
+
+    public function __construct(array $corsConfig)
+    {
+        $this->config = $corsConfig;
+    }
+
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        if ($request->getMethod() === 'OPTIONS') {
+            $response = new \Nyholm\Psr7\Response(204);
+            return $this->addCorsHeaders($response);
+        }
+
+        $response = $handler->handle($request);
+        return $this->addCorsHeaders($response);
+    }
+
+    private function addCorsHeaders(ResponseInterface $response): Response
+    {
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', $this->config['allow_origin'])
+            ->withHeader('Access-Control-Allow-Headers', $this->config['allow_headers'])
+            ->withHeader('Access-Control-Allow-Methods', $this->config['allow_methods']);
+    }
+}
---
+++ test/middleware/CorsMiddlewareTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Test\SSA\middleware;
+
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Server\RequestHandlerInterface;
+use SSA\middleware\CorsMiddleware;
+
+class CorsMiddlewareTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->options = [
+            'allow_origin' => ['https://www.example.com'],
+            'allow_methods' => ['GET, POST, PUT, DELETE'],
+            'allow_headers' => ['Content-Type', 'Authorization'],
+        ];
+    }
+
+    public function testAddsCorsHeadersToNormalRequest()
+    {
+        $middleware = new CorsMiddleware($this->options);
+        $request = new ServerRequest('GET', '/test');
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn(new Response(200));
+
+        $response = $middleware->process($request, $handler);
+
+        $this->assertEquals('*', $response->getHeaderLine('Access-Control-Allow-Origin'));
+        $this->assertStringContainsString('GET', $response->getHeaderLine('Access-Control-Allow-Methods'));
+    }
+
+    public function testHandlesOptionsRequest()
+    {
+        $middleware = new CorsMiddleware($this->options);
+        $request = new ServerRequest('OPTIONS', '/test');
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->expects($this->never())->method('handle');
+
+        $response = $middleware->process($request, $handler);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals('https://www.example.com', $response->getHeaderLine('Access-Control-Allow-Origin'));
+    }
+}
