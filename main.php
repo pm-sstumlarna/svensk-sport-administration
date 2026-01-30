12cb45cc1c208397e0354fb10799038f97152e4d
Peter Myllykoski <peter.myllykoski@situ.se>
2026-01-30 21:48:13 +0100

    Add CORS configuration, organization routes, and conditional signal handling

--- main.php
+++ main.php
@@ -1,12 +1,26 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use React\Http\HttpServer;
 use React\Socket\SocketServer;
 use SSA\Application;
+use SSA\handler\OrganizationHandler;
 
-$configuration = [];
+$configuration = [
+    'cors' => [
+        'allow_origin' => '*',
+        'allow_headers' => 'Content-Type, Authorization',
+        'allow_methods' => 'GET, POST, PUT, DELETE, OPTIONS'
+    ],
+    'authentication' => [],
+    'routes' => [
+        ['/organizations', ['GET', 'POST'], OrganizationHandler::class],
+        ['/organizations/{id}', ['GET', 'PUT', 'DELETE'], OrganizationHandler::class],
+    ],
+    'authorization' => [],
+    'storage' => []
+];
 
 $application = new Application($configuration);
 
@@ -18,8 +32,10 @@ $httpServer->listen($socketServer);
 
 echo "Server running at " . $socketServer->getAddress() . PHP_EOL;
 
-pcntl_signal(SIGTERM, function () use ($httpServer, $socketServer) {
-    $socketServer->close();
-    echo "Socket closed." . PHP_EOL;
-    exit;
-});
+if (function_exists('pcntl_signal')) {
+    pcntl_signal(SIGTERM, function () use ($httpServer, $socketServer) {
+        $socketServer->close();
+        echo "Socket closed." . PHP_EOL;
+        exit;
+    });
+}
